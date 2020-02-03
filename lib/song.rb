f3f35503070116ba4189ef52b8adf65bff4aae43
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-03 16:55:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  
+  def initialize(name, atrist, genre)
+    @@name = name
+    @@atrist = atrist
+    @@genre = genre
+  end
+  
+  def self.count
+    @@count
+  end
+end
